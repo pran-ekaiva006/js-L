6c3354e6a848d302c1d7e21663d61690896f73ee
pran-ekaiva006 <140747573+pran-ekaiva006@users.noreply.github.com>
2025-07-06 18:35:50 +0000

    dataType

---
+++ 01_basic/01_dataType.js
@@ -0,0 +1,24 @@
+"use strict"//treat all js code as newer version
+/*
+alert(2+1)
+we are using nodejs. not browser */
+
+
+let name="pranjal"
+let age=23
+let isLoggedIn=false
+
+
+//number=>2 to power 53
+//bigInt
+//string=>""
+
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=> use for unique
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);//object
