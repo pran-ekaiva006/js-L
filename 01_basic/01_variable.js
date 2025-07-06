bfad29489ef1104699eef6d84a367c87d68002b5
pran-ekaiva006 <140747573+pran-ekaiva006@users.noreply.github.com>
2025-07-06 18:16:55 +0000

    save

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,13 @@
+const accountId=15533
+let accountEmail="shockfurny@gmail.com"
+var accountPassword="12345"
+accountCity="kolkata"
+let accountState;
+
+
+console.log(accountId);
+console.table([accountId,accountPassword,accountEmail,accountCity,accountState])
+/*
+presfer not to use var
+because of issue in block scope and functional scope
+*/
