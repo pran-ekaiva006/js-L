1a532d30f5928e86a5b7a31d37451fa9159584cb
pran-ekaiva006 <140747573+pran-ekaiva006@users.noreply.github.com>
2025-07-06 18:59:07 +0000

    conversion of dataType

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score="pranjal"
+console.log(typeof score)
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33"=>33
+//"33qcd"=>NaN
+//"True"=>1
+//"false"=>0
+
+let isLoggedIn="dd"
+
+let booleanIsloogedIn=Boolean(isLoggedIn);
+console.log(booleanIsloogedIn)
+
+
+/*{for Boolean
+//1=>true
+//0=>false
+//""=>flase
+//"njdj"=>true}
+*/
