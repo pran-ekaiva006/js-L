805873f6591df5572730f3d5a14b62009df97c7b
pran-ekaiva006 <140747573+pran-ekaiva006@users.noreply.github.com>
2025-07-07 17:02:40 +0000

    chhhhh

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,9 +1,9 @@
-let score="pranjal"
-console.log(typeof score)
+// let score="pranjal"
+// console.log(typeof score)
 
-let valueInNumber=Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 //"33"=>33
@@ -11,10 +11,10 @@ console.log(valueInNumber);
 //"True"=>1
 //"false"=>0
 
-let isLoggedIn="dd"
+// let isLoggedIn="dd"
 
-let booleanIsloogedIn=Boolean(isLoggedIn);
-console.log(booleanIsloogedIn)
+// let booleanIsloogedIn=Boolean(isLoggedIn);
+// console.log(booleanIsloogedIn)
 
 
 /*{for Boolean
@@ -23,3 +23,32 @@ console.log(booleanIsloogedIn)
 //""=>flase
 //"njdj"=>true}
 */
+
+
+// let someNumber=33
+// let stringNumber=String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+
+//************************ Operation **********/
+let value=3;
+let negValue=-value
+// console.log(negValue)
+
+
+let str1="hello"
+let str2=" pranjal"
+let str3=str1+str2;
+// console.log(str3)
+// console.log("1"+2)
+// console.log("1"+2+1)
+// console.log(2+1+"2")
+
+
+console.log(+true) //=>output 1
+// console.log(true+) //=>output error
+console.log(+"") //=>output 0
+
+
