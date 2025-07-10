022b132ff2ab00236565f34f238acf4195cdc791
pran-ekaiva006 <140747573+pran-ekaiva006@users.noreply.github.com>
2025-07-10 17:57:04 +0000

    change in location of file

--- 01_basic/dataType_summery.js
+++ 01_basic/dataType_summery.js
@@ -3,8 +3,33 @@
 // 7 type: string,Number,Boolean,null,undefined,symbol,BigInt
 
 
+const Score=100
+const Myscore=100.2
+ const isloggedIn=false
+const OutsideTemp=null
+let UserEmail;
+ 
 
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id===anotherId)
+
+const bigNumber=1388373688999999999999999n
+
+console.log(typeof bigNumber)
 
 //reference(Non primitive)
 
 //array, objects,function
+
+const name=["Ravi","Pankaj","Ayush","Dipak","Yashwant"]
+
+let myobj={
+    name:"Pranjal",
+    age:22
+}
+
+const myfunction=function(){
+    console.log("hello World")
+}
+console.log(typeof myobj)
