23b4d7da590027466ab1acb68bbf6bad223d3ed3
pran-ekaiva006 <140747573+pran-ekaiva006@users.noreply.github.com>
2025-07-24 19:00:08 +0000

    learn math

---
+++ 01_basic/06_numbers.js
@@ -0,0 +1,39 @@
+const score=400
+
+const balance=new Number(100)
+// console.log(balance)
+// console.log(score)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+const otherNumber=225.3446
+
+// console.log(otherNumber.toPrecision(3))
+
+const num=1000000
+// console.log(num.toLocaleString('en-IN'))
+
+
+
+//+++++++++++++++ MATHS ++++++++++++++++++++//
+
+
+// console.log(Math);
+
+// console.log(Math.abs(-12.0003))
+// console.log(Math.round(5.6))
+// console.log(Math.ceil(4.3))
+// console.log(Math.floor(5.6))
+// console.log(Math.min(2,3,4,5,1,5,3,0))
+// console.log(Math.max(2,3,4,5,1,5,3,0))
+
+
+console.log(Math.random())
+console.log(Math.random()*10+1);
+console.log(Math.floor(Math.random()*10+1))
+
+const min=10
+const max=69
+
+console.log(Math.floor(Math.random()*(max-min+1))+min) 
