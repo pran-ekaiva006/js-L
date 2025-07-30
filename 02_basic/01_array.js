467899898d3fe7023d0756893f6b607f8e48194c
pran-ekaiva006 <140747573+pran-ekaiva006@users.noreply.github.com>
2025-07-30 16:00:49 +0000

    n

---
+++ 02_basic/01_array.js
@@ -0,0 +1,42 @@
+//***** pranjal */
+
+//******** Array */
+
+const myArr=[0,1,2,3,4,5,6]
+ const myArr2= new Array(1,2,3,4,5);
+
+ // Array methods
+
+//  myArr.push(8)
+//  console.log(myArr)
+//  myArr.pop()
+//  console.log(myArr)
+
+// myArr.unshift(111)
+
+// console.log(myArr)
+// myArr.shift()
+// console.log(myArr)
+
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(9))
+// console.log(myArr.indexOf(4))
+
+// const newArr=myArr.join()
+
+// console.log(myArr)
+// console.log(typeof newArr)
+
+
+
+//Slice,Splice
+
+console.log("A",myArr);
+
+const myn1=myArr.slice(1,3)
+console.log(myn1)
+console.log("B",myArr);
+
+const myn2=myArr.splice(1,3)
+console.log("c",myArr);
+console.log(myn2)
