5a4a5c20efe469abb7272094a35e3ea9314848df
Pranjal Kumar Verma <140747573+pran-ekaiva006@users.noreply.github.com>
2025-08-26 17:10:40 +0000

    while

---
+++ 05_loop/two.js
@@ -0,0 +1,16 @@
+let index=0;
+
+// while (index<=10) {
+//    console.log(`value of index ${index}`)
+//    index=index+2
+// }
+
+//do while
+let score=11;
+do{
+
+   console.log(`score is ${score}`);
+   score++
+}
+while (score<=10) ;
+    
